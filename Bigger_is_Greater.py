eb35a64af922720b4bfb8228d2e9721951eb8e60
Patrick Liu <llgc@umich.edu>
2017-03-14 23:32:53 -0700

    Add faster solution to Bigger_is_Greater.py

--- Bigger_is_Greater.py
+++ Bigger_is_Greater.py
@@ -3,14 +3,40 @@ class Solution(object):
         """Find a lexicographically gereater anagram for the input string"""
         # convert letters to ascii array
         ord_array = [ord(letter) for letter in list(foo)]
+        # find largest non-increasing suffix
+        i = len(ord_array) - 1
+        # note: <=, not <
+        while i > 0 and ord_array[i] <= ord_array[i - 1]:
+            i -= 1
+        if i <= 0:
+            return 'no answer'
+        # Find pivoting point
+        j = len(ord_array) - 1
+        while ord_array[j] <= ord_array[i - 1]:
+            j -= 1
+        ord_array[j], ord_array[i - 1] = ord_array[i - 1], ord_array[j]
+
+        # reverse suffix, since the suffix is still in order
+        ord_array[i:] = ord_array[i:][::-1]
+        # convert back to list of chars
+        chr_array = [chr(i) for i in ord_array]
+        return ''.join(chr_array)
+    def find_min_greater_anagram2(self, foo):
+        """Slower alternative to find_min_greater_anagram()"""
+        # convert letters to ascii array
+        ord_array = [ord(letter) for letter in list(foo)]
         diffs = [(ord_array[i] - ord_array[i-1]) for i in range(1, len(ord_array))]
         # find the first pair of neighboring letters in increasing order
         loc = -1
+        #print(diffs)
         for i in range(len(diffs)):
             # search in reverse order
             t_loc = len(diffs) - 1 - i
+            #print('t_loc: {}'.format(t_loc))
+            #print('diffs[t_loc]: {}'.format(diffs[t_loc]))
             if diffs[t_loc] > 0:
                 loc = t_loc
+                #print('loc: {}'.format(loc))
                 break
         if loc < 0:
             return 'no answer'
@@ -27,7 +53,7 @@ class Solution(object):
             # convert back to list of chars
             chr_array = [chr(i) for i in ord_array]
             return ''.join(chr_array)
-        
+
 if __name__ == "__main__":
     sln = Solution()
     m = int(input().strip())
